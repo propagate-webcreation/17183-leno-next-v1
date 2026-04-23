2430d69fad54b84a7abba9af6a40782b321e039c
propagate1 <info@propagateinc.com>
2026-04-23 10:30:57 +0900

    chore: remove GA4 + CVR tracking (practice_repo_webcreate_engine)
    - source: bots/ga_tag/cleanup_cancelled

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,15 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="bg-base text-text antialiased">
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-W98RVBKZ');`}
-        </Script>
-      
-<Script src="/scripts/ga4-cta-tracking.js" strategy="afterInteractive" />
+        <Script src="/scripts/ga4-cta-tracking.js" strategy="afterInteractive" />
 <Script src="/tracker.js" strategy="afterInteractive" />{children}</body>
     </html>
   );
